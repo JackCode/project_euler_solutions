c13876985fa96bea4e0c2114f6837471a67cb752
epicrijks <epicrijks@gmail.com>
2020-08-15 20:29:30 -0400

    fix isPrime with sqrt

--- 10001st_prime.cc
+++ 10001st_prime.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 
 bool isPrime(int n) {
   bool isPrime = true;
@@ -6,7 +7,7 @@ bool isPrime(int n) {
   if (n == 0 || n == 1) {
       isPrime = false;
   } else {
-    for (int i = 2; i <= n / 2; ++i) {
+    for (int i = 2; i <= sqrt(n); ++i) {
         if (n % i == 0) {
             isPrime = false;
             break;
